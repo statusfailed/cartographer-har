d37e20996d97544f7cec9ef86f918e1f91d5a17f
statusfailed <sf@statusfailed.com>
2021-05-10 17:44:10 +0100

    add all planned benchmarks

--- benchmarks/circuits.py
+++ benchmarks/circuits.py
@@ -9,6 +9,9 @@ class Gate(Enum):
     DISCARD = 2 # !
     AND     = 3
     XOR     = 4
+    ZERO    = 5
+    ONE     = 6
+    NOT     = 7
 
     @property
     def type(self):
@@ -16,7 +19,10 @@ class Gate(Enum):
             Gate.COPY: (1, 2),
             Gate.DISCARD: (1, 0),
             Gate.AND: (2, 1),
-            Gate.XOR: (2, 1)
+            Gate.XOR: (2, 1),
+            Gate.ZERO: (0, 1),
+            Gate.ONE: (0, 1),
+            Gate.NOT: (1, 1)
         }
 
         return arities[self]
@@ -39,53 +45,93 @@ def add_mul():
     return copy2() >> (Gate.XOR.HAR @ Gate.AND.HAR)
 
 def full_adder():
+    """ full_adder() constructs a 2-bit full adder:
+
+            C_in ---\         /--- Result
+                     \-|---|-/
+            A    ------| + |
+                     /-|---|-\
+            B    ---/         \--- Cout
+
+    """
     f = add_mul()
     id = HAR.identity(1)
     return (f @ id) >> (id @ HAR.twist(1, 1)) >> (f @ id) >> (id @ f) >> (id @ Gate.XOR.HAR)
 
 def adder(n):
-    """ adder(n) constructs a 2^n bit adder
-    """
-    circuit = full_adder()
-    for i in range(0, power):
-        pass
+    """ adder(n) constructs a 2^n-bit adder """
+    # NOTE: strictly speaking this assumes the input data is in a particular
+    # order which is not how you'd normally implement things- but one can
+    # definitely consider this an n-bit adder.
+    if n < 0:
+        raise ValueError("Can't construct an n-bit adder for n < 0")
+    if n == 0:
+        # If an n-bit full adder is (Cin × n × n) → (n × Cout) then this makes sense :-)
+        return HAR.identity(1)
+    elif n == 1:
+        return full_adder()
+    else:
+        id = HAR.identity(2**(n - 1))
+        a = adder(n - 1) # recurse
+        return (a @ id @ id) >> (id @ a)
+    
+    return circuit
 
-class BasicSuite:
-    def time_identity(self):
-        HAR.identity(1)
+###############################
+# Benchmarks
 
-    def time_identity_tensor_identity(self):
-        HAR.identity(1) @ HAR.identity(1) @ HAR.identity(1) @ HAR.identity(1)
+MAX_LOG2_SIZE = 20
 
-    def time_identity_tensor_identity_v2(self):
-        (HAR.identity(1) @ HAR.identity(1)) @ (HAR.identity(1) @ HAR.identity(1))
+class HarBenchmark:
+    params = list(range(0, MAX_LOG2_SIZE + 1))
+    param_names = ['n']
 
-class TensorTime:
-    # log_2(size) of circuits to tensor together
-    params = list(range(0, 21))
-    # params = list(range(0, 6)) # dense- 14.9s @ 5(!!!)
-    param_names = ['log_2(n)']
-
+class TensorTime(HarBenchmark):
     def setup(self, n):
-        self.lhs = full_adder()
-        self.rhs = full_adder()
+        acc = Gate.AND.HAR
         for i in range(0, n):
-            self.lhs = self.lhs @ self.rhs
-            self.rhs = self.rhs @ self.rhs
+            acc = acc @ acc
+        self.lhs = acc
+        self.rhs = acc
 
     def time_tensor(self, n):
         self.lhs @ self.rhs
 
-class CircuitSuite:
-    # params = [ 2**i for i in range(0, 8) ]
-    params = list(range(0, 12))
 
+class ComposeSmallBoundary(HarBenchmark):
+    def setup(self, n):
+        acc = Gate.NOT.HAR
+        for i in range(0, n):
+            acc = acc >> acc
+        self.lhs = acc
+        self.rhs = acc
+
+    def time_small_boundary(self, n):
+        self.lhs >> self.rhs
+
+class ComposeLargeBoundary:
+    def setup(self, n):
+        acc = Gate.NOT.HAR
+        for i in range(0, n):
+            acc = acc @ acc
+        self.lhs = acc
+        self.rhs = acc
+
+    def time_tensor(self, n):
+        self.lhs >> self.rhs
+
+class AdderCircuit:
+    def setup(self, n):
+        pass
+
+    # def time_
+
+class SyntheticBenchmark(HarBenchmark):
     def setup(self, n):
         # compute the basic building block before running the benchmark
-        self.full_adder = full_adder()
+        self.adder = adder(n)
 
-    def time_full_adder_chain(self, n):
-        # Compute 2^n tensors of a full adder block
-        a = self.full_adder
-        for i in range(0, n):
-            a = a @ a
+    # Construct an n-bit adder from two n-1 bit adders.
+    def time_n_bit_adder(self, n):
+        id = HAR.identity(2**n)
+        return (self.adder @ id @ id) >> (id @ self.adder)
